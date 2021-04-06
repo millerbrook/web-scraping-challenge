1145c1b36e3f506389bafa087831b4d653ba26a6
Brook Miller <millerbrook@gmail.com>
2021-04-06 11:06:42 -0400

    Minor Fix to Table Formatting

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -9,71 +9,74 @@ def init_browser():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     return Browser('chrome', **executable_path, headless=False)
 
+
 def scrape():
     browser = init_browser()
     scrape_dict = {}
-    #Visit Nasa's Mars News Site
+    # Visit Nasa's Mars News Site
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
-    #Begin scrape 1
-    #Scrape page into Soup
+    # Begin scrape 1
+    # Scrape page into Soup
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
-    #Get headline and brief paragraph text
+    # Get headline and brief paragraph text
     result = soup.find('div', class_='image_and_description_container')
     title = result.find('h3').text
     para = result.find('div', class_='rollover_description_inner').text
-    #print(para)
-    #print(title)
-    scrape_dict['news_title']=title
+    # print(para)
+    # print(title)
+    scrape_dict['news_title'] = title
     scrape_dict['news_para'] = para
-    #Begin scrape 2
+    # Begin scrape 2
     # Setup splinter
     #executable_path = {'executable_path': ChromeDriverManager().install()}
     #browser = Browser('chrome', **executable_path, headless=False)
-    url= 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
     browser.visit(url)
-    #Scrape page into soup
+    # Scrape page into soup
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
-    #Grab featured JPL image
+    # Grab featured JPL image
     result = soup.find('img', class_="fade-in")
     src = result['src']
 
     featured_image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/"+src
-    scrape_dict['featured_image_url']=featured_image_url
-    #print(featured_image_url)
-    #Begin 3rd scrape (w/ Pandas)
+    scrape_dict['featured_image_url'] = featured_image_url
+    # print(featured_image_url)
+    # Begin 3rd scrape (w/ Pandas)
     url = "https://space-facts.com/mars/"
     tables = pd.read_html(url)[0]
     tables = pd.DataFrame(tables)
-    tables.set_index(0)
+    tables = tables.rename({0: "Feature", 1: "Data"}, axis=1)
+    tables = tables.set_index("Feature")
+    # tables.set_index(0)
     tables_html = tables.to_html()
     tables_html
     scrape_dict['tables_html'] = tables_html
 
-    #Begin 4th Scrape
-    url= 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    # Begin 4th Scrape
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
-    #Loop through 4 Links and Do Queries in Individual Web Pages
+    # Loop through 4 Links and Do Queries in Individual Web Pages
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
     result = soup.find('div', class_="collapsible results")
     items = result.find_all('div', class_='item')
 
-    #Create list for individual pictures urls and loop through 'items'
+    # Create list for individual pictures urls and loop through 'items'
     pic_list = []
     for item in items:
         link = item.find('a', class_='itemLink')
         pic_url = link['href']
         pic_list.append(pic_url)
 
-    #Go to individual pages for hemisphere photos and grab urls to append to dictionary
+    # Go to individual pages for hemisphere photos and grab urls to append to dictionary
     browser.quit()
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
-    url= 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
@@ -81,10 +84,11 @@ def scrape():
     results = soup.find_all('div', class_="description")
     click_list = []
     for item in results:
-        pic_url = 'https://astrogeology.usgs.gov' + item.find_all('a')[0]['href']
+        pic_url = 'https://astrogeology.usgs.gov' + \
+            item.find_all('a')[0]['href']
         click_list.append(pic_url)
         title = item.find('h3').text
-        hemispheres.append({'title':title})
+        hemispheres.append({'title': title})
 
     pic_list = []
 
